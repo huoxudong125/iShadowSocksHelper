52afb0217dcba00f915a30927a5283866dbf650b
huoxudong125 <423huo@163.com>
2016-01-12 22:04:08 +0800

    Add Restart the process Shadowsocks.

--- GetShadowSocksPWD/Program.cs
+++ GetShadowSocksPWD/Program.cs
@@ -1,22 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using ScrapySharp.Extensions;
 using ScrapySharp.Html;
 using ScrapySharp.Network;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text;
 
 namespace GetShadowSocksPWD
 {
     internal class Program
     {
         private static readonly string configfile = "gui-config.json";
+
         private static void Main(string[] args)
         {
             GenrateFreeServersConfig();
+            ReStartShadowSocks();
 
+            Console.WriteLine("Enter any key to exit.");
             Console.Read();
         }
 
@@ -82,7 +87,7 @@ namespace GetShadowSocksPWD
 
                 Console.WriteLine("Serialize the config to file.");
                 var path = "server.txt";
-                WriteServersToFile(rootObject,path);
+                WriteServersToFile(rootObject, path);
 
                 Console.WriteLine("Update the gui-config.json");
 
@@ -98,14 +103,66 @@ namespace GetShadowSocksPWD
             }
         }
 
-        private static void WriteServersToFile(RootObject rootObject,string path)
+        private static void WriteServersToFile(RootObject rootObject, string path)
         {
             //var str = JsonHelper.FormatJson(JsonHelper.Serialize(rootObject));
-            var str = JsonConvert.SerializeObject(rootObject,Formatting.Indented);
-            
+            var str = JsonConvert.SerializeObject(rootObject, Formatting.Indented);
+
             using (var file = new StreamWriter(path, false, Encoding.UTF8))
             {
                 file.Write(str);
+            }
+        }
+
+        private static void ReStartShadowSocks()
+        {
+            try
+            {
+                var exeFile = "shadowsocks";
+
+                Console.WriteLine("Try to kill the process [{0}]", exeFile);
+                //kill the process
+                Process.GetProcesses()
+                             .Where(x => x.ProcessName.ToLower()
+                                          .StartsWith(exeFile))
+                             .ToList().ForEach(p => KillProcessAndChildren(p.Id));
+                //.ForEach(x => x.Kill());
+
+                Console.WriteLine("{1}Start the process [{0}]", exeFile, Environment.NewLine);
+                
+                //start
+                Process.Start(exeFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:{0}", ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Kill a process, and all of its children, grandchildren, etc.
+        /// </summary>
+        /// <param name="pid">Process ID.</param>
+        private static void KillProcessAndChildren(int pid)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher
+              ("Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection moc = searcher.Get();
+            foreach (ManagementObject mo in moc)
+            {
+                KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
+            }
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+
+                proc.Kill();
+                Console.WriteLine("Kill Process :{0}", proc.ProcessName);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Kill Process error:{0}", ex.Message);
+                // Process already exited.
             }
         }
     }
